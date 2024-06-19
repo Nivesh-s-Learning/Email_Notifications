e343e7783adb225ebd0b7ae39238fe3768bfb9ad
Nivesh108 <sharmanivesh08@gmail.com>
2024-06-19 16:24:08 +0530

    python script changes

--- send_email.py
+++ send_email.py
@@ -1,9 +1,6 @@
 import smtplib
-
 from email.mime.multipart import MIMEMultipart
-
 from email.mime.text import MIMEText
-
 import os
 
  
@@ -11,59 +8,34 @@ import os
 def send_email(to_emails, subject, body):
 
     smtp_server = 'smtp.gmail.com'
-
     smtp_port = 587
-
     username = os.getenv('SMTP_USERNAME')
-
     password = os.getenv('SMTP_PASSWORD')
-
     from_email = 'sharmanivesh08@gmail.com'
 
-   
+    try:
+        # Set up the SMTP server
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(username, password)
 
-    # Set up the SMTP server
+        # Create the email
+        msg = MIMEMultipart()
+        msg['From'] = from_email
+        msg['To'] = to_emails
+        msg['Subject'] = subject
+        msg.attach(MIMEText(body, 'plain'))
 
-    server = smtplib.SMTP(smtp_server, smtp_port)
-
-    server.starttls()
-
-    server.login(username, password)
-
-   
-
-    # Create the email
-
-    msg = MIMEMultipart()
-
-    msg['From'] = from_email
-
-    msg['To'] = to_emails
-
-    msg['Subject'] = subject
-
-    msg.attach(MIMEText(body, 'plain'))
-
-   
-
-    # Send the email
-
-    server.send_message(msg)
-
-    server.quit()
-
- 
+        # Send the email
+        server.send_message(msg)
+        server.quit()
+    except Exception as e:
+        print(f"Error sending email: {str(e)}")
 
 if __name__ == "__main__":
-
     to_emails = os.getenv('mails')
-
     # Print the email addresses for log in github actions
-
     print(f"Sending email to: {to_emails}")
-
     subject = "Please commit everyday"
-
     body = "Please commit everyday."
-
     send_email(to_emails, subject, body)
